78d91526d629aac6e74b177a42a2ceefd13511f3
Yixiao <yixiaozhouchen@hotmail.com>
2020-05-04 11:09:26 -0400

    server  build up

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); // allow us to parse json
+
+//connect to mongo database
+const uri = process.env.DB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('MongoDB database connection established successfully');
+})
+
+app.listen(port , ()=>{
+    console.log(`Server is running on port: ${port}`);
+})
+
